92e66b42bbb7b86cb7439cbdd4472337420e715a
Elizabeth Rogers <beth3346@gmail.com>
2018-04-29 13:20:26 -0500

    use elr-scss-accordion css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import accordion from './js/elr-accordion'
-import './css/main.css'
-import './css/accordion.css'
+import accordion from './js/elr-accordion';
+import './css/main.css';
+import '../node_modules/elr-scss-accordion/dist/css/accordion.css';
 
-accordion.init()
+accordion.init();
